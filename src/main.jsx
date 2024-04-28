c631ec85120b300834a85b13bec3f8ac6a1d1f3a
3KR4 <3KR4@GMAIL.COM>
2024-04-28 17:29:36 +0200

    5

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Home from './pages/Home';
 import Product from './pages/Product';
+import Shop from './pages/shop';
+
 import ScrollToTop from './components/scrollToTop';
 
 const AppRoutes = () => {
@@ -14,6 +16,7 @@ const AppRoutes = () => {
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
           <Route path="/product/:productName" element={<Product />} />
+          <Route path="/shop" element={<Shop />} />
         </Route>
       </Routes>
     </BrowserRouter>
